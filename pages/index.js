b9ba9e1dc93c1bb439ca930f63f0ef1d6012eccf
Eashver Elango <eashvere@gmail.com>
2023-10-14 20:38:54 -0700

    fixes

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,17 @@
 import Head from "next/head";
 import { useState } from "react";
 import { useEffect } from "react";
-import styles from "./index.module.css";
+import "bulma/css/bulma.min.css";
 import onSubmit from "./api/generate";
 
 export default function Home() {
-  const [result, setResult] = useState();
+  const [inputText, setInputText] = useState('');
   const [chatState, setChatState] = useState([]);
   const [currentName, setCurrentName] = useState("");
+  const [friends, setFriends] = useState([]);
   const [currentScenarioIndex, setCurrentScenarioIndex] = useState("");
 
-
   useEffect(() => {
-    setChatState([...chatState, {"role": "user", "content": "Hello."}, {"role": "assistant", "content": "Hello."},  {"role": "user", "content": "What is your name?"}]);
     setCurrentName("John");
     setCurrentScenarioIndex(0);
   }, []);
@@ -31,11 +30,13 @@ export default function Home() {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      console.log("asdf")
-      const data = await onSubmit(chatState, currentName, currentScenarioIndex); // Call onSubmit with the user's input
+      var dummyChatState = [...chatState, inputText]
+      setChatState(dummyChatState)
+      console.log("chatState:" + chatState)
+      const data = await onSubmit(dummyChatState, currentName, currentScenarioIndex); // Call onSubmit with the user's input
       console.log(data);
-      setResult(data);
-
+      dummyChatState = [...dummyChatState, data]
+      setChatState(dummyChatState)
     } catch (error) {
       console.error(error);
       alert(error.message);
@@ -60,8 +61,8 @@ export default function Home() {
                 <div className="select">
                   <select>
                     <option>Select a Friend</option>
-                    {friends.map((friend) => (
-                      <option>{friend}</option>
+                    {friends.map((friend, i) => (
+                      <option key={i}>{friend}</option>
                     ))}
                   </select>
                 </div>
@@ -72,6 +73,7 @@ export default function Home() {
             <div className="p-4">
               <p className="title">Playground</p>
               <p className="subtitle">Playground area</p>
+              <button onClick={handleSubmit}>Start Conversation</button>
             </div>
             <div
               className="p-4 is-justify-content-flex-end is-flex is-flex-direction-column is-small"
@@ -79,29 +81,45 @@ export default function Home() {
             >
               <p className="title">Chat</p>
               <div className="p-2">
-                {
-                    messages.map((m, i) => {
-                        const msgClass = i === 0 || i % 2 === 0 // for demo purposes, format every other msg
-                        return (
-                          <p style={{ padding: '.25em', textAlign: msgClass ? 'left' : 'right', overflowWrap: 'normal' }}>
-                            <span key={i} className={`tag is-medium ${msgClass ? 'is-success' : 'is-info'}`}>{m}</span>
-                          </p>
-                        )}
-                    )
-                }
+                {chatState.map((m, i) => {
+                  const msgClass = m.role === "assistant"; // for demo purposes, format every other msg
+                  return (
+                    <p
+                      style={{
+                        padding: ".25em",
+                        textAlign: msgClass ? "left" : "right",
+                        overflowWrap: "normal",
+                      }}
+                    >
+                      <span
+                        key={i}
+                        className={`tag is-medium ${
+                          msgClass ? "is-success" : "is-info"
+                        }`}
+                      >
+                        {m.content}
+                      </span>
+                    </p>
+                  );
+                })}
               </div>
-              <div className="field has-addons">
-                <div className="control is-expanded">
-                  <input
-                    className="input"
-                    type="text"
-                    placeholder="How would you respond?"
-                  />
+              <form onSubmit={handleSubmit}>
+                <div className="field has-addons">
+                  <div className="control is-expanded">
+                    <input
+                      className="input"
+                      type="text"
+                      placeholder="How would you respond?"
+                      value={inputText}
+                      onChange={(e) => setInputText(e.target.value)}
+                      disabled={chatState.length === 0}
+                    />
+                  </div>
+                  <div className="control">
+                    <a className="button is-success" onClick={handleSubmit}>Submit</a>
+                  </div>
                 </div>
-                <div className="control">
-                  <a className="button is-success">Submit</a>
-                </div>
-              </div>
+              </form>
             </div>
           </div>
         </div>
