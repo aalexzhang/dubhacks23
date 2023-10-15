2f6ef97ea7803190dc03c32462046c7d569ed6c4
Eashver Elango <eashvere@gmail.com>
2023-10-14 21:24:09 -0700

    more fixes

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import "bulma/css/bulma.min.css";
 import onSubmit from "./api/generate";
 
 export default function Home() {
+  const [aiLoading, setaiLoading] = useState(false);
   const [inputText, setInputText] = useState('');
   const [chatState, setChatState] = useState([]);
   const [currentName, setCurrentName] = useState("");
@@ -27,20 +28,42 @@ export default function Home() {
     }
   };
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+  const startConversation = async () => {
+    setaiLoading(true);
     try {
-      var dummyChatState = [...chatState, inputText]
+      var dummyChatState = [...chatState, {"role": "user", "content": ''}]
       setChatState(dummyChatState)
       console.log("chatState:" + chatState)
       const data = await onSubmit(dummyChatState, currentName, currentScenarioIndex); // Call onSubmit with the user's input
       console.log(data);
-      dummyChatState = [...dummyChatState, data]
+      dummyChatState = [...dummyChatState, {"role": "assistant", "content": data}]
       setChatState(dummyChatState)
     } catch (error) {
       console.error(error);
       alert(error.message);
     }
+    setaiLoading(false);
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (inputText.trim() === '') {
+      return
+    }
+    setaiLoading(true);
+    try {
+      var dummyChatState = [...chatState, {"role": "user", "content": inputText.trim()}]
+      setChatState(dummyChatState)
+      console.log("chatState:" + chatState)
+      const data = await onSubmit(dummyChatState, currentName, currentScenarioIndex); // Call onSubmit with the user's input
+      console.log(data);
+      dummyChatState = [...dummyChatState, {"role": "assistant", "content": data}]
+      setChatState(dummyChatState)
+    } catch (error) {
+      console.error(error);
+      alert(error.message);
+    }
+    setaiLoading(false);
   };
 
   return (
@@ -73,7 +96,7 @@ export default function Home() {
             <div className="p-4">
               <p className="title">Playground</p>
               <p className="subtitle">Playground area</p>
-              <button onClick={handleSubmit}>Start Conversation</button>
+              <button onClick={startConversation}>Start Conversation</button>
             </div>
             <div
               className="p-4 is-justify-content-flex-end is-flex is-flex-direction-column is-small"
@@ -81,7 +104,7 @@ export default function Home() {
             >
               <p className="title">Chat</p>
               <div className="p-2">
-                {chatState.map((m, i) => {
+                {chatState.filter((m) => m.content !== '').map((m, i) => {
                   const msgClass = m.role === "assistant"; // for demo purposes, format every other msg
                   return (
                     <p
@@ -89,6 +112,7 @@ export default function Home() {
                         padding: ".25em",
                         textAlign: msgClass ? "left" : "right",
                         overflowWrap: "normal",
+                        maxWidth: "100px"
                       }}
                     >
                       <span
@@ -107,18 +131,22 @@ export default function Home() {
                 <div className="field has-addons">
                   <div className="control is-expanded">
                     <input
-                      className="input"
+                      className={`input ${inputText.trim() === '' ? 'is-danger': ''}`}
                       type="text"
                       placeholder="How would you respond?"
                       value={inputText}
                       onChange={(e) => setInputText(e.target.value)}
-                      disabled={chatState.length === 0}
+                      disabled={chatState.length === 0 || aiLoading}
                     />
                   </div>
                   <div className="control">
-                    <a className="button is-success" onClick={handleSubmit}>Submit</a>
+                    <a className="button is-success" onClick={handleSubmit} disabled={chatState.length === 0 || aiLoading || inputText.trim().length === 0}>Submit</a>
                   </div>
                 </div>
+                {
+                    inputText.trim().length === 0 &&
+                    <p className="help is-danger">Type out your solution!</p>
+                  }
               </form>
             </div>
           </div>
